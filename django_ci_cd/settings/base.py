d7cf4689bfda1eac6e749fa058ca3bdd898cc27d
Vitalii Kostiushko <adkostiushkowords@gmail.com>
2023-12-12 20:25:39 +0200

    split settings

--- django_ci_cd/settings/base.py
+++ django_ci_cd/settings/base.py
@@ -76,16 +76,7 @@ WSGI_APPLICATION = 'django_ci_cd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',  # Use the service name from docker-compose.yml
-        'PORT': '5432',
-    }
-}
+
 
 CACHES = {
     'default': {
